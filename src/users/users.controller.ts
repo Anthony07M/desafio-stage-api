276b05fe1256a9cb6cc9d844e001dea44486110d
Anthony <anthonyfreitas2015@gmail.com>
2023-03-22 13:52:07 -0400

    refactor: UsersFilters controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,12 +12,12 @@ import { PrismaFilter } from 'src/errors/prisma/prisma.filter';
 import { UserCreateDto, UserOutputDto } from './dto/users.dto/users.dto';
 import { UsersService } from './users.service';
 
+@UseFilters(PrismaFilter)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @UseFilters(PrismaFilter)
   async create(@Body() data: UserCreateDto): Promise<UserOutputDto> {
     return await this.usersService.create(data);
   }
@@ -29,13 +29,11 @@ export class UsersController {
 
   @HttpCode(204)
   @Delete(':id')
-  @UseFilters(PrismaFilter)
   async delete(@Param('id') id: string): Promise<void> {
     return await this.usersService.delete(id);
   }
 
   @Get(':id')
-  @UseFilters(PrismaFilter)
   async find(@Param('id') id: string): Promise<UserOutputDto> {
     return this.usersService.find(id);
   }
